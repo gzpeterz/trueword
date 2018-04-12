c12abb0e249ad44489f47c8173881747977f6b21
vicky <15096093909@163.com>
2018-04-12 10:20:22 +0800

    vicky2

--- src/com/hc/trueword/Test.java
+++ src/com/hc/trueword/Test.java
@@ -1,9 +1,14 @@
 package com.hc.trueword;
 
+import com.hc.vicky.VickyGroup;
+
 public class Test {
 
 	public static void main(String[] args) {
-
+		VickyGroup group = new VickyGroup();
+		String name = group.getName();
+		System.out.println(group.getTrueWord(name));
+		
 	}
 
 }
